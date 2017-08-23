161ced4a1f38e166790746d5793aeaab4f7f9e51
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2017-08-23 22:47:36 +0200

    add mocha plugin

--- test-mocha-chai.js
+++ test-mocha-chai.js
@@ -1,15 +1,37 @@
 module.exports = {
+  env: {
+    mocha: true
+  },
+
+  plugins: [
+    'mocha'
+  ],
+
   settings: {
     'import/core-modules': [
       'chai'
     ]
   },
 
-  env: {
-    mocha: true
-  },
-
   rules: {
-    'no-unused-expressions': 'off'
+    'handle-done-callback': 'error',
+    'max-top-level-suites': ['error', {
+      limit: 1
+    }],
+    'no-exclusive-tests': 'error',
+    'no-global-tests': 'error',
+    'no-hooks': 'off',
+    'no-hooks-for-single-case': 'off',
+    'no-identical-title': 'error',
+    'no-mocha-arrows': 'error',
+    'no-nested-tests': 'error',
+    'no-pending-tests': 'error',
+    'no-return-and-callback': 'error',
+    'no-sibling-hooks': 'error',
+    'no-skipped-tests': 'warn',
+    'no-synchronous-tests': 'off',
+    'no-top-level-hooks': 'error',
+    'valid-suite-description': 'off',
+    'valid-test-description': 'off'
   }
 }
