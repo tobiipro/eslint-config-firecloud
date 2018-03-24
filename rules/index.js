9600f5519efdec7b44739d76d7298ad52560456c
Ian Savchenko (isao) <ian.savchenko@tobii.com>
2018-03-24 21:17:45 +0100

    Added new rules to the list of exported

--- rules/index.js
+++ rules/index.js
@@ -2,5 +2,7 @@ module.exports.rules = {
   'order-imports': require('./order-imports'),
   'import-specifier-newline': require('./import-specifier-newline'),
   'import-specifier-curly-newline': require('./import-specifier-curly-newline'),
-  'padding-line-import-multiple': require('./padding-line-import-multiple')
+  'padding-line-import-multiple': require('./padding-line-import-multiple'),
+  'underscore-prefix-non-exported': require('./underscore-prefix-non-exported'),
+  'no-underscore-prefix-exported': require('./no-underscore-prefix-exported')
 }
