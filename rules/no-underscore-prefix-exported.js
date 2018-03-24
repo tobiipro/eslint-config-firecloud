63b3d9783c9bdc11b9e62784815fa4e008be7a69
Ian Savchenko (isao) <ian.savchenko@tobii.com>
2018-03-24 21:03:00 +0100

    no-underscore-prefix-exported rule added along with tests

---
+++ rules/no-underscore-prefix-exported.js
@@ -0,0 +1,54 @@
+let _ = require('lodash');
+
+module.exports = {
+  meta: {
+    docs: {
+      description: 'forbid top-level exported (public) variables to be prefixed with underscore',
+      category: 'Stylistic Issues',
+      recommended: false
+    },
+
+    schema: [
+      {
+        type: 'object',
+        additionalProperties: false
+      }
+    ]
+  },
+
+  create(context) {
+    const sourceCode = context.getSourceCode();
+
+    let checkVariable = function(variable) {
+      let scope = variable.scope.type;
+      if (scope !== 'module' && scope !== 'global') {
+        // not top-level scope
+        return;
+      }
+
+      if (!_.startsWith(variable.name, '_')) {
+        return;
+      }
+
+      context.report({
+        node: variable.defs[0].name,
+        message: 'Top-level exported variables should not be prefixed with "_"'
+      });
+    }
+
+    let checkDeclaration = function(node) {
+      if (node.parent.type !== 'ExportNamedDeclaration') {
+        // not exported variable
+        return;
+      }
+
+      let vars = context.getDeclaredVariables(node);
+      _.forEach(vars, checkVariable);
+    }
+
+    return {
+      VariableDeclaration: checkDeclaration,
+      FunctionDeclaration: checkDeclaration
+    };
+  }
+};
---
+++ test/no-underscore-prefix-exported.test.js
@@ -0,0 +1,25 @@
+
+const rule = require("../rules/no-underscore-prefix-exported"),
+RuleTester = require("eslint").RuleTester;
+
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2015, sourceType: 'module' } });
+
+ruleTester.run("no-underscore-prefix-exported", rule, {
+  valid: [
+    'export let a = 4;',
+    'export let {b} = a;',
+    'export function f(b, c){let a = 3}',
+    'export var {[yo]: weird} = a;',
+    'let _a = 5;',
+    'let {b} = a',
+    'var {b: d} = a',
+    'function _f(b, c){let a = 3}'
+  ],
+  invalid: [
+    { code: 'export let _a = 5', errors: 1 },
+    { code: 'export let {_a} = 5', errors: 1 },
+    { code: 'export let {a:_b} = 5', errors: 1 },
+    { code: 'export function _a(b, c){let d = 5}', errors: 1 },
+    { code: 'export let {[b]:_c}', errors: 1 }
+  ]
+});
