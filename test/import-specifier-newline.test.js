6f22563d4487cc941aff041c8565a74acc099d94
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2018-03-25 19:14:22 +0200

    prettify tests

--- test/import-specifier-newline.test.js
+++ test/import-specifier-newline.test.js
@@ -12,72 +12,118 @@ const errorMessageAllowMultiple =
 const errorMessage = 'Import specifiers must go on a new line.';
 
 ruleTester.run('import-specifier-newline', rule, {
-  valid: [
-    {
-      code: 'import {a} from "b"',
-      options: [{allowMultiplePerLine: false}]
-    },
-    {
-      code: 'import {\na\n} from "b"',
-      options: [{allowMultiplePerLine: false}]
-    },
-    {
-      code: 'import {\na,\nb\n} from "b"',
-      options: [{allowMultiplePerLine: false}]
-    },
-    {
-      code: 'import {a,\nb} from "b"',
-      options: [{allowMultiplePerLine: false}]
-    },
+  valid: [{
+    code: 'import {a} from "b"',
+    options: [{
+      allowMultiplePerLine: false
+    }]
+  }, {
+    code: [
+      'import {',
+      'a',
+      '} from "b"'
+    ].join('\n'),
+    options: [{
+      allowMultiplePerLine: false
+    }]
+  }, {
+    code: [
+      'import {',
+      'a,',
+      'b',
+      '} from "b"'
+    ].join('\n'),
+    options: [{
+      allowMultiplePerLine: false
+    }]
+  }, {
+    code: [
+      'import {a,',
+      'b} from "b"'
+    ].join('\n'),
+    options: [{
+      allowMultiplePerLine: false
+    }]
+  }, {
+    code: 'import {a} from "b"',
+    options: [{
+      allowMultiplePerLine: true
+    }]
+  }, {
+    code: 'import {a,b,c} from "b"',
+    options: [{
+      allowMultiplePerLine: true
+    }]
+  }, {
+    code: [
+      'import {a,',
+      'b,',
+      'c} from "b"'
+    ].join('\n'),
+    options: [{
+      allowMultiplePerLine: true
+    }]
+  }],
 
-    {
-      code: 'import {a} from "b"',
-      options: [{allowMultiplePerLine: true}]
-    },
-    {
-      code: 'import {a,b,c} from "b"',
-      options: [{allowMultiplePerLine: true}]
-    },
-    {
-      code: 'import {a,\nb,\nc} from "b"',
-      options: [{allowMultiplePerLine: true}]
-    }
-  ],
-  invalid: [
-    {
-      code: 'import {a,b} from "b"',
-      options: [{allowMultiplePerLine: false}],
-      errors: [
-        {message: errorMessage}
-      ],
-      output: 'import {a,\nb} from "b"'
-    },
-    {
-      code: 'import {a,\nb,c} from "b"',
-      options: [{allowMultiplePerLine: false}],
-      errors: [
-        {message: errorMessage}
-      ],
-      output: 'import {a,\nb,\nc} from "b"'
-    },
-
-    {
-      code: 'import {a,b,\nc} from "b"',
-      options: [{allowMultiplePerLine: true}],
-      errors: [
-        {message: errorMessageAllowMultiple}
-      ],
-      output: 'import {a,\nb,\nc} from "b"'
-    },
-
+  invalid: [{
+    code: 'import {a,b} from "b"',
+    options: [{
+      allowMultiplePerLine: false
+    }],
+    errors: [{
+      message: errorMessage
+    }],
+    output: [
+      'import {a,',
+      'b} from "b"'
+    ].join('\n')
+  }, {
+    code: [
+      'import {a,',
+      'b,c} from "b"'
+    ].join('\n'),
+    options: [{
+      allowMultiplePerLine: false
+    }],
+    errors: [{
+      message: errorMessage
+    }],
+    output: [
+      'import {a,',
+      'b,',
+      'c} from "b"'
+    ].join('\n')
+  }, {
+    code: [
+      'import {a,b,',
+      'c} from "b"'
+    ].join('\n'),
+    options: [{
+      allowMultiplePerLine: true
+    }],
+    errors: [{
+      message: errorMessageAllowMultiple
+    }],
+    output: [
+      'import {a,',
+      'b,',
+      'c} from "b"'
+    ].join('\n')
+  }, {
     // no fixes if comments are present
-    {
-      code: 'import {a,/*a comment*/b,\nc} from "b"',
-      options: [{allowMultiplePerLine: true}],
-      errors: [
-        {message: errorMessageAllowMultiple}
-      ],
-      output: 'import {a,/*a comment*/b,\nc} from "b"'
-    }
-  ]
+    code: [
+      'import {a,/*a comment*/b,',
+      'c} from "b"'
+    ].join('\n'),
+    options: [{
+      allowMultiplePerLine: true
+    }],
+    errors: [{
+      message: errorMessageAllowMultiple
+    }],
+    output: [
+      'import {a,/*a comment*/b,',
+      'c} from "b"'
+    ].join('\n')
+  }]
 });
--- test/no-underscore-prefix-exported.test.js
+++ test/no-underscore-prefix-exported.test.js
@@ -9,21 +9,38 @@ const ruleTester = new eslint.RuleTester({
 });
 
 ruleTester.run('no-underscore-prefix-exported', rule, {
-  valid: [
-    'export let a = 4;',
-    'export let {b} = a;',
-    'export function f(b, c){let a = 3}',
-    'export var {[yo]: weird} = a;',
-    'let _a = 5;',
-    'let {b} = a',
-    'var {b: d} = a',
-    'function _f(b, c){let a = 3}'
-  ],
-  invalid: [
-    {code: 'export let _a = 5', errors: 1},
-    {code: 'export let {_a} = 5', errors: 1},
-    {code: 'export let {a:_b} = 5', errors: 1},
-    {code: 'export function _a(b, c){let d = 5}', errors: 1},
-    {code: 'export let {[b]:_c}', errors: 1}
-  ]
+  valid: [{
+    code: 'export let a = 4;'
+  }, {
+    code: 'export let {b} = a;'
+  }, {
+    code: 'export function f(b, c){let a = 3}'
+  }, {
+    code: 'export var {[yo]: weird} = a;'
+  }, {
+    code: 'let _a = 5;'
+  }, {
+    code: 'let {b} = a'
+  }, {
+    code: 'var {b: d} = a'
+  }, {
+    code: 'function _f(b, c){let a = 3}'
+  }],
+
+  invalid: [{
+    code: 'export let _a = 5',
+    errors: 1
+  }, {
+    code: 'export let {_a} = 5',
+    errors: 1
+  }, {
+    code: 'export let {a:_b} = 5',
+    errors: 1
+  }, {
+    code: 'export function _a(b, c){let d = 5}',
+    errors: 1
+  }, {
+    code: 'export let {[b]:_c}',
+    errors: 1
+  }]
 });
