50e7db85e3e9812c30a951c45c406bcc61e9ef6b
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2018-03-25 09:00:29 +0200

    law and order

--- configs/firecloud.js
+++ configs/firecloud.js
@@ -7,9 +7,9 @@ module.exports = {
     'firecloud/import-specifier-newline': ['warn', {
       allowMultiplePerLine: false
     }],
+    'firecloud/no-underscore-prefix-exported': 'off',
     'firecloud/order-imports': 'warn',
     'firecloud/padding-line-import-multiple': 'error',
-    'firecloud/no-underscore-prefix-exported': 'off',
     'firecloud/underscore-prefix-non-exported': 'off'
   }
 };
--- rules/index.js
+++ rules/index.js
@@ -1,8 +1,8 @@
 module.exports.rules = {
-  'order-imports': require('./order-imports'),
-  'import-specifier-newline': require('./import-specifier-newline'),
   'import-specifier-curly-newline': require('./import-specifier-curly-newline'),
+  'import-specifier-newline': require('./import-specifier-newline'),
+  'no-underscore-prefix-exported': require('./no-underscore-prefix-exported'),
+  'order-imports': require('./order-imports'),
   'padding-line-import-multiple': require('./padding-line-import-multiple'),
-  'underscore-prefix-non-exported': require('./underscore-prefix-non-exported'),
-  'no-underscore-prefix-exported': require('./no-underscore-prefix-exported')
+  'underscore-prefix-non-exported': require('./underscore-prefix-non-exported')
 }
