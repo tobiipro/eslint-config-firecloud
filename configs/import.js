6632c28717b7dd6ad996a9ea718c911294f50d84
Andrei Neculau <andrei.neculau@gmail.com>
2019-01-20 11:00:40 +0100

    upgrade eslint-plugin-import

--- configs/import.js
+++ configs/import.js
@@ -11,14 +11,15 @@ module.exports = {
   },
 
   rules: {
+    'import/dynamic-import-chunkname': 'off',
     'import/export': 'error',
     'import/exports-last': 'off',
     'import/extensions': ['error', {
       js: 'never',
       json: 'always'
     }],
+    'import/first': 'error',
     'import/group-exports': 'off',
-    'import/imports-first': 'error',
     'import/max-dependencies': 'off',
     'import/named': 'error',
     'import/namespace': ['error', {
@@ -29,19 +30,26 @@ module.exports = {
     'import/no-amd': 'error',
     'import/no-anonymous-default-export': 'off',
     'import/no-commonjs': 'off',
+    'import/no-cycle': 'error',
+    'import/no-default-export': 'off',
     'import/no-deprecated': 'error',
     'import/no-duplicates': 'error',
     'import/no-extraneous-dependencies': 'error',
+    'import/no-internal-modules': 'off',
     'import/no-mutable-exports': 'off',
     'import/no-named-as-default-member': 'off',
     'import/no-named-as-default': 'off',
     'import/no-namespace': 'off',
     'import/no-nodejs-modules': 'off',
+    'import/no-relative-parent-imports': 'off',
     'import/no-restricted-paths': 'off',
     'import/no-self-import': 'error',
+    'import/no-unassigned-import': 'error',
     'import/no-unresolved': 'error',
+    'import/no-webpack-loader-syntax': 'error',
     'import/no-useless-path-segments': 'error',
     'import/order': 'off',
-    'import/prefer-default-export': 'warn'
+    'import/prefer-default-export': 'warn',
+    'import/unambiguous': 'off'
   }
 };
