d9914a9b1ade83076b6c94e92c2bd6ad4ff0334c
Andrei Neculau <andrei.neculau@gmail.com>
2019-02-15 13:31:59 +0100

    handle function({a} = {}) cases

--- rules/util.js
+++ rules/util.js
@@ -1,26 +1,51 @@
 module.exports = {
+  // eslint-disable-next-line complexity
   isNodeOnlyParamObject: function(node) {
     let isArgument =
-          node.type === 'ObjectExpression' &&
-          node.parent &&
-          (
-            node.parent.type === 'CallExpression' ||
-            node.parent.type === 'NewExpression'
-          );
+        node.type === 'ObjectExpression' &&
+        node.parent &&
+        (
+          node.parent.type === 'CallExpression' ||
+          node.parent.type === 'NewExpression'
+        );
+
+    let isOnlyArgument =
+        isArgument &&
+        node.parent.arguments &&
+        node.parent.arguments.length === 1;
 
     let isParam =
-          node.type === 'ObjectPattern' &&
-          node.parent &&
+        node.type === 'ObjectPattern' &&
+        node.parent &&
+        (
+          node.parent.type === 'ArrowFunctionExpression' ||
+          node.parent.type === 'FunctionDeclaration' ||
+          node.parent.type === 'FunctionExpression' ||
           (
-            node.parent.type === 'ArrowFunctionExpression' ||
-            node.parent.type === 'FunctionDeclaration' ||
-            node.parent.type === 'FunctionExpression'
-          );
+            node.parent.type === 'AssignmentPattern' &&
+            node.parent.parent &&
+            (
+              node.parent.parent.type === 'ArrowFunctionExpression' ||
+              node.parent.parent.type === 'FunctionDeclaration' ||
+              node.parent.parent.type === 'FunctionExpression'
+            )
+          )
+        );
 
     let isOnlyParam =
-          (isArgument && node.parent.arguments.length === 1) ||
-          (isParam && node.parent.params.length === 1);
+        isParam &&
+        (
+          (
+            node.parent.type === 'AssignmentPattern' &&
+            node.parent.parent.params &&
+            node.parent.parent.params.length === 1
+          ) ||
+          (
+            node.parent.params &&
+            node.parent.params.length === 1
+          )
+        );
 
-    return isOnlyParam;
+    return isOnlyArgument || isOnlyParam;
   }
 };
