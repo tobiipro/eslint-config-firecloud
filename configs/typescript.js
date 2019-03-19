8acb72750daf0172db0e3dd01865e797970d983a
Andrei Neculau <andrei.neculau@gmail.com>
2019-03-19 08:01:05 +0100

    sync typescript with basic

--- configs/typescript.js
+++ configs/typescript.js
@@ -2,31 +2,16 @@
 // please update README.md with required version of the eslint plugin
 // when introducing breaking changes
 
+let _basic = require('./basic');
+
 module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended'
   ],
 
   rules: {
-    // should be in sync with basic.js
-    '@typescript-eslint/camelcase': ['warn', {
-      properties: 'never'
-    }],
-    '@typescript-eslint/indent': ['error', 2, {
-      VariableDeclarator: {
-        var: 2,
-        let: 2,
-        const: 3
-      }
-    }],
-    '@typescript-eslint/no-unused-vars': ['error', {
-      args: 'all',
-      argsIgnorePattern: '^_',
-      caughtErrors: 'all',
-      caughtErrorsIgnorePattern: '^_',
-      ignoreRestSiblings: true,
-      vars: 'all',
-      varsIgnorePattern: '^_'
-    }]
+    '@typescript-eslint/camelcase': _basic.rules.camelcase,
+    '@typescript-eslint/indent': _basic.rules.indent,
+    '@typescript-eslint/no-unused-vars': _basic.rules['no-unused-vars']
   }
 };
