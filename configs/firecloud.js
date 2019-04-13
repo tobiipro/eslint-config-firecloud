4183e3ae8708aab31b14971e44f0cbd39484d572
Andrei Neculau <andrei.neculau@gmail.com>
2019-04-13 21:55:09 +0200

    firecloud rules: explicitly mark those that it overrides

--- configs/firecloud.js
+++ configs/firecloud.js
@@ -4,20 +4,26 @@ module.exports = {
   ],
 
   rules: {
+    'array-bracket-newline': 'off',
     'firecloud/array-bracket-newline': ['error', {
       multiline: true,
       minItems: 1,
       allowObjectCurly: true
     }],
+
+    'array-element-newline': 'off',
     'firecloud/array-element-newline': ['error', {
       multiline: true,
       minItems: 1,
       allowObjectCurly: true
     }],
+
     'firecloud/import-specifier-newline': 'error',
     'firecloud/no-const': 'error',
     'firecloud/no-for': 'error',
     'firecloud/no-underscore-prefix-exported': 'error',
+
+    'object-curly-newline': 'off',
     'firecloud/object-curly-newline': ['error', {
       OnlyParam: {
         multiline: true,
@@ -45,10 +51,13 @@ module.exports = {
         consistent: true
       }
     }],
+
+    'object-property-newline': 'off',
     'firecloud/object-property-newline': ['error', {
       allowAllPropertiesOnSameLine: false,
       allowOnlyParamOnSameLine: true
     }],
+
     'firecloud/order-imports': 'error',
     'firecloud/padding-line-import-multiple': 'error',
     'firecloud/underscore-prefix-non-exported': 'error'
