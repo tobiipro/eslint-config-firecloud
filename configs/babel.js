09308e6a5e9f114e0311d0af6bc3ec904aab4122
Andrei Neculau <andrei.neculau@gmail.com>
2019-04-13 18:06:46 +0200

    bring back eslint-babel-plugin

--- configs/babel.js
+++ configs/babel.js
@@ -1,7 +1,39 @@
+let _basic = require('./basic');
+
 module.exports = {
   parser: 'babel-eslint',
 
   parserOptions: {
     sourceType: 'module'
+  },
+
+  plugins: [
+    'babel'
+  ],
+
+  rules: {
+    'new-cap': 'off',
+    'babel/new-cap': _basic.rules['new-cap'],
+
+    camelcase: 'off',
+    'babel/camelcase': _basic.rules.camelcase,
+
+    'no-invalid-this': 'off',
+    'babel/no-invalid-this': _basic.rules['no-invalid-this'],
+
+    'object-curly-spacing': 'off',
+    'babel/object-curly-spacing': _basic.rules['object-curly-spacing'],
+
+    quotes: 'off',
+    'babel/quotes': _basic.rules.quotes,
+
+    semi: 'off',
+    'babel/semi': _basic.rules.semi,
+
+    'no-unused-expressions': 'off',
+    'babel/no-unused-expressions': _basic.rules['no-unused-expressions'],
+
+    'valid-typeof': 'off',
+    'babel/valid-typeof': _basic.rules['valid-typeof']
   }
 };
