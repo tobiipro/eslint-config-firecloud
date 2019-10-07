c2b08d98a7b526bedf3621e1d5457786d094c496
Andrei Neculau <andrei.neculau@gmail.com>
2019-10-07 13:11:25 +0200

    upgrade typescript plugin

--- configs/typescript.js
+++ configs/typescript.js
@@ -1,6 +1,6 @@
 // NOTE that this is an addon config. An eslint plugin needs to be installed manually.
 
-let _pluginVsn = '^1.6.0';
+let _pluginVsn = '^1.13.0';
 let _pluginName = '@typescript-eslint/eslint-plugin';
 let _pluginActualVsn = require(`${_pluginName}/package.json`).version;
 let _semver = require('semver');
@@ -11,6 +11,7 @@ if (!_semver.satisfies(_pluginActualVsn, _pluginVsn)) {
 
 let _ = require('lodash');
 let _basic = require('./basic');
+let _babel = require('./babel');
 
 module.exports = {
   extends: [
@@ -28,20 +29,20 @@ module.exports = {
   },
 
   rules: {
-    // 'no-var' is broken atm for typescript, but it will be fixed in a the next version
-    // see https://github.com/eslint/eslint/pull/11443
-    'no-var': 'off',
-
     // rules in tslint
+    // actual tslint rule name follows as a comment, IFF different than @typescript-eslint rule name
+
     '@typescript-eslint/adjacent-overload-signatures': 'error',
     '@typescript-eslint/array-type': ['error', 'array'],
-    // '@typescript-eslint/await-thenable': 'error', // tslint:await-promise
+    '@typescript-eslint/await-thenable': 'error', // tslint:await-promise
     '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/ban-types': 'off',
 
-    '@typescript-eslint/no-type-alias': 'off', // tslint:interface-over-type-literal
-    '@typescript-eslint/prefer-interface': 'off', // tslint:interface-over-type-literal
+    '@typescript-eslint/no-type-alias': 'error', // tslint:interface-over-type-literal
+    // DEPRECATED // '@typescript-eslint/prefer-interface': 'off', // tslint:interface-over-type-literal
+    '@typescript-eslint/consistent-type-definitions': 'off', // tslint:interface-over-type-literal
 
+    '@typescript-eslint/class-name-casing': 'error', // tslint:class-name
     '@typescript-eslint/explicit-member-accessibility': 'off', // tslint:member-access
     '@typescript-eslint/generic-type-naming': 'error',
     '@typescript-eslint/interface-name-prefix': 'error', // tslint:interface-name
@@ -51,22 +52,29 @@ module.exports = {
     '@typescript-eslint/no-angle-bracket-type-assertion': 'error',
     '@typescript-eslint/no-empty-interface': 'error',
     '@typescript-eslint/no-explicit-any': 'off', // tslint:no-any
+    '@typescript-eslint/no-floating-promises': 'error',
     '@typescript-eslint/no-inferrable-types': 'off',
     '@typescript-eslint/no-misused-new': 'error',
     '@typescript-eslint/no-namespace': 'error',
     '@typescript-eslint/no-non-null-assertion': 'error',
     '@typescript-eslint/no-object-literal-type-assertion': 'error',
     '@typescript-eslint/no-parameter-properties': 'error',
+
     // typescript specific but not marked typescriptOnly
-    '@typescript-eslint/no-triple-slash-reference': 'error', // tslint:no-reference
+    // DEPRECATED // '@typescript-eslint/no-triple-slash-reference': 'error', // tslint:no-reference
+    '@typescript-eslint/triple-slash-reference': 'error', // tslint:no-reference-import
+
     '@typescript-eslint/no-unnecessary-qualifier': 'error',
     '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    '@typescript-eslint/prefer-for-of': 'error',
     '@typescript-eslint/prefer-function-type': 'off', // tslint:callable-types
     '@typescript-eslint/prefer-namespace-keyword': 'error', // tslint:no-internal-module
+    '@typescript-eslint/prefer-readonly': 'error',
     '@typescript-eslint/promise-function-async': ['error', {
       allowAny: true
     }], // requiresTypeInfo
     '@typescript-eslint/restrict-plus-operands': 'error', // requiresTypeInfo
+    '@typescript-eslint/strict-boolean-expressions': 'error',
     '@typescript-eslint/type-annotation-spacing': ['error', { // tslint:typedef-whitespace
       'call-signature': 'nospace',
       'index-signature': 'nospace',
@@ -80,12 +88,31 @@ module.exports = {
       'property-declaration': 'onespace',
       'variable-declaration': 'onespace'
     }],
-    // '@typescript-eslint/unbound-method': 'error', // tslint:no-unbound-method
+    '@typescript-eslint/unbound-method': 'error', // tslint:no-unbound-method
     '@typescript-eslint/unified-signatures': 'error',
 
     // -------------------------------------------------------------------------
 
-    // rules not in tslint, typescript specific
+    // rules in tslint (overriding eslint rules)
+
+    'consistent-this': 'off',
+    '@typescript-eslint/no-this-alias': _basic.rules['consistent-this'], // tslint:no-this-assignment
+
+    // marked as, but not really typescriptOnly
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': _basic.rules['no-use-before-define'], // tslint:no-use-before-declare
+
+    'no-unnecessary-class': undefined,
+    '@typescript-eslint/no-extraneous-class': 'error', // tslint: no-unnecessary-class
+
+    // marked as, but not really typescriptOnly
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': _basic.rules['no-unused-vars'], // tslint:no-unused-variable
+
+    // -------------------------------------------------------------------------
+
+    // rules not in tslint, specific to typescript-language
+
     '@typescript-eslint/member-delimiter-style': 'off', // 'error', // FIXME
     '@typescript-eslint/member-naming': ['error', {
       private: '^_'
@@ -93,18 +120,23 @@ module.exports = {
 
     // -------------------------------------------------------------------------
 
-    // not typescript specific
+    // rules not in tslint, not specific to typescript-language
 
-    camelcase: 'off',
-    '@typescript-eslint/camelcase': _basic.rules.camelcase,
-
-    '@typescript-eslint/class-name-casing': 'error', // tslint:class-name
+    // 'camelcase': 'off',
+    'babel/camelcase': 'off',
+    '@typescript-eslint/camelcase': _babel.rules.camelcase,
 
     '@typescript-eslint/explicit-function-return-type': ['error', {
       allowExpressions: true,
       allowTypedFunctionExpressions: true,
       allowHigherOrderFunctions: true
     }],
+
+    'func-call-spacing': 'off',
+    '@typescript-eslint/func-call-spacing': _basic.rules['func-call-spacing'],
+
+    'global-require': 'off',
+    '@typescript-eslint/no-require-imports': _basic.rules['global-require'],
 
     indent: 'off',
     // eslint-disable-next-line no-sparse-arrays
@@ -117,29 +149,29 @@ module.exports = {
     'no-array-constructor': 'off',
     '@typescript-eslint/no-array-constructor': _basic.rules['no-array-constructor'],
 
-    // 'no-unnecessary-class': undefined, // @typescript-eslint/no-extraneous-class
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': _basic.rules['no-empty-function'],
 
-    'global-require': 'off',
-    '@typescript-eslint/no-require-imports': _basic.rules['global-require'],
+    'no-extra-parens': 'off',
+    '@typescript-eslint/no-extra-parens': _basic.rules['no-extra-parens'],
 
-    'consistent-this': 'off',
-    '@typescript-eslint/no-this-alias': _basic.rules['consistent-this'], // tslint:no-this-assignment
+    'no-magic-numbers': 'off',
+    '@typescript-eslint/no-magic-numbers': _basic.rules['no-magic-numbers'],
 
-    // marked as, but not really typescriptOnly
-    // '@typescript-eslint/no-var-requires': 'error',
+    '@typescript-eslint/no-misused-promises': 'error',
 
-    // marked as, but not really typescriptOnly
-    'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': _basic.rules['no-use-before-define'], // tslint:no-use-before-declare
-
-    // marked as, but not really typescriptOnly
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': _basic.rules['no-unused-vars'], // tslint:no-unused-variable
+    '@typescript-eslint/no-var-requires': 'error',
 
     'no-useless-constructor': 'off',
     '@typescript-eslint/no-useless-constructor': _basic.rules['no-useless-constructor'],
 
-    // @typescript-eslint/prefer-for-of
+    '@typescript-eslint/prefer-includes': 'error',
+    '@typescript-eslint/prefer-regexp-exec': 'error',
+    '@typescript-eslint/prefer-string-starts-ends-with': 'error',
+
+    // DISABLED because it conflicts with '@typescript-eslint/promise-function-async'
+    // 'require-await': 'off',
+    // '@typescript-eslint/require-await': _basic.rules['require-await'],
 
     // -------------------------------------------------------------------------
 
