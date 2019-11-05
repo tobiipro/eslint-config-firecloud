5d6c55686c827e4b8c5dfd8a2d0526e307e4c90d
Andrei Neculau <andrei.neculau@gmail.com>
2019-11-05 21:55:44 +0100

    'jsdoc/require-return-type': 'off'

--- configs/typescript.js
+++ configs/typescript.js
@@ -185,6 +185,7 @@ module.exports = {
     'jsdoc/require-param-description': 'error',
     'jsdoc/require-param-type': 'off',
     'jsdoc/require-param': 'off',
-    'jsdoc/require-returns': 'off'
+    'jsdoc/require-returns': 'off',
+    'jsdoc/require-returns-type': 'off'
   }
 };
