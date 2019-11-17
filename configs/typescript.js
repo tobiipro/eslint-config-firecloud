770d6328188419fe7a80ac22c191d7cc11759d0c
Andrei Neculau <andrei.neculau@gmail.com>
2019-11-17 12:33:06 +0100

    clarify rule comment

--- configs/typescript.js
+++ configs/typescript.js
@@ -183,10 +183,10 @@ module.exports = {
 
     // -------------------------------------------------------------------------
 
-    // bad for typescript, lowers type inference
     // NOTE we assume eslint-plugin-lodash is still loaded
 
     'lodash/prefer-noop': 'off',
+    // bad for typescript, lowers type inference
     'lodash/prop-shorthand': ['error', 'never'],
 
     // -------------------------------------------------------------------------
