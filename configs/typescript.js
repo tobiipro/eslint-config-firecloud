33d5aca4b64b37a3480aa6d3c22750b98a2bc76d
Fredrik Johansson Torneus <fredrik.j.torneus@gmail.com>
2021-02-16 16:26:12 +0100

    Changes required to support typescript-eslint 4.15

--- configs/typescript.js
+++ configs/typescript.js
@@ -1,7 +1,7 @@
 // NOTE that this is an addon config. An eslint plugin needs to be installed manually.
 
 let _pluginName = '@typescript-eslint/eslint-plugin';
-let _pluginVsn = '^2.6.0';
+let _pluginVsn = '^4.15.0';
 let _pluginActualVsn = require(`${_pluginName}/package.json`).version;
 let _semver = require('semver');
 
