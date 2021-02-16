58bc9e42e6b26d7d61ae978236a404214f57e00a
Fredrik Johansson Torneus <fredrik.j.torneus@gmail.com>
2021-02-16 14:49:13 +0100

    disable eslint

--- configs/typescript-eslint-recommended.spec.js
+++ configs/typescript-eslint-recommended.spec.js
@@ -1,40 +1,39 @@
-const _restoreBasicOverrides = require('./typescript-eslint-recommended');
 
-jest.mock('@typescript-eslint/eslint-plugin/dist/configs/recommended.json', () => {
-  return {
-    rules: {
-      '@typescript-eslint/remove-this-rule': 'error',
-      'keep-this-rule': 'recommended-value',
-      'recommended-specific-rule': 'recommended-value',
-  }
-  }
-}, {virtual: true})
+let _restoreBasicOverrides = require('./typescript-eslint-recommended');
 
-jest.mock('./basic', () => {
-  return {
-    rules: {
-      'keep-this-rule': 'basic-value',
-      'basic-specific-rule': 'basic-value'
-    }
+jest.mock('@typescript-eslint/eslint-plugin/dist/configs/recommended.json', () => ({
+  rules: {
+    '@typescript-eslint/remove-this-rule': 'error',
+    'keep-this-rule': 'recommended-value',
+    'recommended-specific-rule': 'recommended-value'
   }
-})
+}), {
+  virtual: true
+});
+
+jest.mock('./basic', () => ({
+  rules: {
+    'keep-this-rule': 'basic-value',
+    'basic-specific-rule': 'basic-value'
+  }
+}));
 
 describe('typescript-eslint-recommended', () => {
   it('should remove all rules in the recommended file which starts with @typescript-eslint', () => {
     expect(_restoreBasicOverrides.rules).toStrictEqual(
       expect.not.objectContaining({'@typescript-eslint/remove-this-rule': 'error'})
-    )
-  })
+    );
+  });
 
   it('should change the value of rules which exist in both recommended and basic to using the value in basic', () => {
     expect(_restoreBasicOverrides.rules).toStrictEqual(
       expect.objectContaining({'keep-this-rule': 'basic-value'})
-    )
-  })
+    );
+  });
 
   it('should remove the value of rules which only exist in recommended and not in basic', () => {
     expect(_restoreBasicOverrides.rules).toStrictEqual(
       expect.objectContaining({'recommended-specific-rule': undefined})
-    )
-  })
-})
+    );
+  });
+});
